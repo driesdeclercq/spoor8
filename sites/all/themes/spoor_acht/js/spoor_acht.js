d580ed8ceb0a36d0fcb7bd23733ad17979910fe3
Dries Declercq <dries.declercq@gmail.com>
2013-02-23 21:21:20 +0100

    Sesssion detail theming and extra modules fo the content type.

--- sites/all/themes/spoor_acht/js/spoor_acht.js
+++ sites/all/themes/spoor_acht/js/spoor_acht.js
@@ -6,20 +6,26 @@
   Drupal.breakpoints = {};
   Drupal.breakpoints.small = 480;
   Drupal.breakpoints.medium = 768;
-  Drupal.breakpoints.large = 960;
+  Drupal.breakpoints.large = 1024;
 
   Drupal.mobileToolbar = {};
   Drupal.mobileToolbar.loaded = false;
-  Drupal.mobileToolbar.types = 'search navigation contact';
+  Drupal.mobileToolbar.types = 'search navigation';
+
+  Drupal.spoorAcht = {};
 
  // Drupal.slideShow = {};
-
-
   $(document).ready(function() {
+    Drupal.spoorAcht.setContactToggle();
+
   //  Drupal.slideShow.element = $('.html-carousel .view-content');
 
+//var trimmedText =+ $(this).text().substr(0, 800).lastIndexOf(" ");
+//      var shortText = $(this).text().trim().substring(0, sumMotherfukkentext).split(" ").slice(0, -1).join(" ") + "...";
 
-    $('#session-carousel').carouFredSel({
+
+/* Start carousel */
+$('#session-carousel').carouFredSel({
       responsive: true,
       circular: false,
       auto: false,
@@ -58,22 +64,7 @@
       $(this).addClass('selected');
       return false;
     });
-
-
-    var closeButton = $('<div id="contact-close">close</div>').click(function(){
-      $(this)
-        .parents(".block-webform")
-        .animate({'margin-right': '-500px'}
-      );
-    });
-    $("#contact .block-webform .content").append(closeButton);
-    $("#contact .block-toggle-contact .content div").click(function(){
-        $(this)
-          .parents(".block-toggle-contact")
-          .next()
-          .animate({'margin-right': 0}
-        );
-    });
+/* End carousel */
 
     // Initialise the mobile toolbar.
     Drupal.mobileToolbar.initialise();
@@ -90,8 +81,19 @@
     }
   });
 
-  $(window).resize(
-    function() {
+/*
+$(window).resize(function() {
+    if ($(window).width() >= Drupal.breakpoints.medium) {
+      // Show or initialize the slideshow.
+  //    Drupal.slideShow.enable();
+    }
+    else {
+      // Hide the slideshow.
+  //    Drupal.slideShow.element.hide();
+    }
+  });
+*/
+  $(window).resize(function() {
       if ($(window).width() < Drupal.breakpoints.medium) {
         // Initialise or enable mobile menu.
         if (Drupal.mobileToolbar.loaded & $("#mobile-toolbar").hasClass('loaded')) {
@@ -104,12 +106,43 @@
         Drupal.mobileToolbar.disable();
         // Show or initialize the slideshow.
   //      Drupal.slideShow.enable();
+
+          if ($(window).width() < Drupal.breakpoints.large) {
+            // Background on session detail page. Move the background up, so the
+            // bottom will match the shadow on the video.
+            var maxHeight = 304;
+            var currentHeight = $(".field-session-video").height();
+            var backgroundPosition = (currentHeight - maxHeight) + 'px';
+            $("#content-wrapper").css({'background-position-y' : backgroundPosition});
+
+           /* Trim text on session detail.
+            var maxWidth = 399;
+            var maxHeight = 160;
+            var currentWidth = $(".node-session .field-name-title").width();
+            var maxLength = 800;  // width = 1024
+            var theLength = 0;
+            var trimmedText = '';
+            // 1 line = 16px &&  chars;
+
+            $(".node-session .field-name-body p").each( function () {
+              theLength = theLength + $(this).text().length;
+              trimmedText = trimmedText + $(this).text();
+            });
+
+            maxLength = $(window).width() / Drupal.breakpoints.large * 0.6 * maxLength;
+            if (theLength > maxLength) {
+              position = trimmedText.substr(0, Math.round(maxLength)).lastIndexOf(" ");
+              var trimmedText = trimmedText.trim().substring(0, position).split(" ").slice(0, -1).join(" ") + "...";
+              $(".node-session .field-name-body").html('<p>' + trimmedText + '</p>');
+            }
+          */
+         }
       }
     }
   );
 
 /*
- * Inlfield labels;
+ * Add Infield labels to the contact form.
  */
   Drupal.behaviors.contactFieldLabels = {
     attach: function (context) {
@@ -120,33 +153,55 @@
     }
   };
 
+  /*
+   * Contact form toggle functionality.
+   */
+  Drupal.spoorAcht.setContactToggle = function() {
+    var contact = $("#contact");
+    var closeButton = $('<div id="contact-close">close</div>').click(function(){
+      contact
+        .toggleClass('toggled')
+        .animate({'width': '30px'}
+      );
+    });
+    $("#contact .block-webform .content").append(closeButton);
+    // Toggle block.
+    $("#contact .block-toggle-contact .content div").click(function(){
+      if (contact.hasClass('toggled')) {
+        contact.animate({'width': '30px'}).toggleClass('toggled');
+      }
+      else {
+        contact.animate({'width': '530px'}).toggleClass('toggled');
+      }
+    });
+  }
+
+  /*
+   * Create the mobile toolbar buttons and content.
+   */
   Drupal.mobileToolbar.initialise = function() {
     // Add the toggle buttons for search and menu to the toolbar.
     var typesArray = Drupal.mobileToolbar.types.split(' ');
     var type = '';
     var buttonDiv = '';
-    var toolbarHtml = '';
+    var toolbarHtml = ''
+    var contactToggle = $("<div>");
+    contactToggle.addClass('clearfix mobile-visible')
+      .attr('id', 'toggle-contact')
+      .html('<div class="toolbar-button-wrapper"><a href="contact" id="contact-link">Contact</a></div></div>')
+      .insertAfter("#toolbar div:first").hide();
 
     for (var i = 0; i < typesArray.length; i++) {
       type = typesArray[i];
-      // Prepare toolbar button html. Do not add contact-html.
-      // Contact cannot be duplicate tor it will brake functionality.
-      if (type != 'contact') {
-        toolbarHtml += $('#' + type).html();
-      }
-
+      // Prepare toolbar button html.
+      toolbarHtml += $('#' + type).html();
       // Create the toggle buttons for mobile layout and add to their respective
       // regions.
       buttonDiv = $("<div>")
-        .addClass('toolbar-button-wrapper')
+        .addClass('toolbar-button-wrapper mobile-visible')
         .attr('id', 'toggle-' + type)
         .html('<div class="toolbar-button" id="btn-' + type + '">Toggle ' + type + '</div>');
-      if (type != 'contact') {
          buttonDiv.appendTo($('#' + type)).hide();
-      }
-      else {
-        buttonDiv.appendTo($('#toolbar')).hide();
-      }
     }
 
     // Fill mobile toolbar.
@@ -154,18 +209,16 @@
       .html(toolbarHtml)
       .addClass('loaded');
 
-    // Add menu toggle functionality.
+    // Add mobile toggle functionality.
     $('.toolbar-button').click(function() {
+      // Add the current type as class-name.
       var currentToggle = $(this).attr('id');
       var className = currentToggle.substr(4);
       // The mobile toolbar is hidden, show it and set toggled type class.
       if ($("#mobile-toolbar").hasClass('hidden')) {
-        $("#mobile-toolbar").addClass(className)
+        $("#mobile-toolbar")
+          .addClass(className)
           .removeClass('hidden');
-        // If type == contact: show.
-        if(className == 'contact') {
-          $("#contact").show();
-        }
       }
       else {
         // Toggled item is visible, hide the mobile toolbar.
@@ -173,23 +226,14 @@
           $("#mobile-toolbar")
             .removeClass(Drupal.mobileToolbar.types)
             .addClass('hidden');
-          // if  type == contact: hide.
-          if(className == 'contact') {
-            $("#contact").hide();
-          }
         }
         else {
-          // Switch what is visible.
+          // Other item is visible, switch content.
           // Remove all type classes from mobile toolbar and set toggled type
           // class.
-          $("#mobile-toolbar").removeClass(Drupal.mobileToolbar.types)
+          $("#mobile-toolbar")
+            .removeClass(Drupal.mobileToolbar.types)
             .addClass(className);
-          if(className == 'contact') {
-            $("#contact").show();
-          }
-          else {
-            $("#contact").hide();
-          }
         }
       }
     });
@@ -204,41 +248,32 @@
    */
   Drupal.mobileToolbar.enable = function() {
     // Show the mobile buttons.
+    $(".mobile-visible").show();
+    // Hide the desktop search and menu.
     $('#search .region-search').hide();
     $('#navigation .region-navigation').hide();
-    $('#contact').hide();
-    $('#contact .block-webform').css({'margin-right': 0});
-    $('#toggle-search').show();
-    $('#toggle-navigation').show();
-    $('#toggle-contact').show();
+    // Hide the destop contact block.
+    $('.region-contact').addClass('hidden');
+    // Switch language switcher styling.
     $(".language-switcher-locale-url li").addClass('toolbar-button-wrapper');
     $(".language-switcher-locale-url li a").addClass('toolbar-button');
   }
 
+  /**
+   * Disable mobile navigation.
+   */
   Drupal.mobileToolbar.disable = function() {
-    // Toggle toolbar elements.
-    $('#contact').show();
-    $('#contact .block-webform').css({'margin-right': '-500px'});
+    // Hide mobile toolbar buttons.
+    $(".mobile-visible").hide();
+    // Show desktop elements.
+    $('.region-contact').removeClass('hidden');
     $('#search .region-search').show();
     $('#navigation .region-navigation').show();
-    $('#toggle-search').hide();
-    $('#toggle-navigation').hide();
-    $('#toggle-contact').hide();
+    // Switch language switcher styling.
     $(".language-switcher-locale-url li").removeClass('toolbar-button-wrapper');
     $(".language-switcher-locale-url li a").removeClass('toolbar-button');
-    // Hide the mobile navigation.
+    // Hide the mobile content.
     $("#mobile-toolbar").addClass('hidden');
   }
-
-  $(window).resize(function() {
-    if ($(window).width() >= Drupal.breakpoints.medium) {
-      // Show or initialize the slideshow.
-  //    Drupal.slideShow.enable();
-    }
-    else {
-      // Hide the slideshow.
-  //    Drupal.slideShow.element.hide();
-    }
-  });
 
 })(jQuery, Drupal);
